790cd1e378075f52ffcc4be6956ae70e45afed23
Pierre Tomasina <tomasina@plab.io>
2016-11-17 09:44:41 +0100

    add abstract class valueObject

---
+++ src/ValueObject.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Plab\ValueObject;
+
+abstract class ValueObject
+{
+    /**
+     * @param string $key
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __get(string $key)
+    {
+        if (!property_exists($this, $key)) {
+            throw new \Exception('Property not exist');
+        }
+
+        return $this->$key;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @throws \Exception
+     */
+    public function __set($key, $value)
+    {
+        throw new \Exception('In ValueObject you are not allowed to setup property');
+    }
+}
